08fee8f53ce8e6c513e2a9685d180ae271bdee13
Nick Palacio <npalacio@unomaha.edu>
2019-01-29 18:28:37 -0600

    Initial commits - config folder

---
+++ config/xboot_conf.c
@@ -0,0 +1,49 @@
+/* conf.c (GENERATED FILE; DO NOT EDIT) */
+
+#include <xinu.h>
+
+
+extern	devcall	ioerr(void);
+extern	devcall	ionull(void);
+
+/* Device independent I/O switch */
+
+struct	dentry	devtab[NDEVS] =
+{
+/**
+ * Format of entries is:
+ * dev-number, minor-number, dev-name,
+ * init, open, close,
+ * read, write, seek,
+ * getc, putc, control,
+ * dev-csr-address, intr-handler, irq
+ */
+
+/* CONSOLE is tty */
+	{ 0, 0, "CONSOLE",
+	  (void *)ttyinit, (void *)ionull, (void *)ionull,
+	  (void *)ttyread, (void *)ttywrite, (void *)ioerr,
+	  (void *)ttygetc, (void *)ttyputc, (void *)ttycontrol,
+	  (void *)0x3f8, (void *)ttydispatch, 42 },
+
+/* NULLDEV is null */
+	{ 1, 0, "NULLDEV",
+	  (void *)ionull, (void *)ionull, (void *)ionull,
+	  (void *)ionull, (void *)ionull, (void *)ioerr,
+	  (void *)ionull, (void *)ionull, (void *)ioerr,
+	  (void *)0x0, (void *)ioerr, 0 },
+
+/* ETHER0 is eth */
+	{ 2, 0, "ETHER0",
+	  (void *)ethinit, (void *)ioerr, (void *)ioerr,
+	  (void *)ethread, (void *)ethwrite, (void *)ioerr,
+	  (void *)ioerr, (void *)ioerr, (void *)ethcontrol,
+	  (void *)0x0, (void *)ethdispatch, 43 },
+
+/* NAMESPACE is nam */
+	{ 3, 0, "NAMESPACE",
+	  (void *)naminit, (void *)namopen, (void *)ioerr,
+	  (void *)ioerr, (void *)ioerr, (void *)ioerr,
+	  (void *)ioerr, (void *)ioerr, (void *)ioerr,
+	  (void *)0x0, (void *)ioerr, 0 }
+};
