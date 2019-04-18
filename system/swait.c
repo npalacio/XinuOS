ba0927755fe54beec9935c0c68af16b23afb626a
Nick Palacio <npalacio@unomaha.edu>
2019-04-17 19:33:55 -0500

    Working on swait

--- system/swait.c
+++ system/swait.c
@@ -9,5 +9,31 @@ syscall swait(
         sid32 semB
         )
 {
+    intmask mask;           /* Saved interrupt mask     */
+    struct  sentry *semptr;     /* Ptr to sempahore table entry */
+
+    // Disable interrupts
+    mask = disable();
+
+    semptrA = &semtab[semA];
+    semptrB = &semtab[semB];
+    // Validate the two semaphores
+    if(semA == semB) {
+        restore(mask);
+        return SYSERR;
+    }
+    if(isbadsem(semA) || isbadsem(semB)) {
+        restore(mask);
+        return SYSERR;
+    }
+    if(semptrA->sstate == S_FREE || semptrB->sstate == S_FREE) {
+        restore(mask);
+        return SYSERR;
+    }
+
+    // If both semaphores are 'good to go' restore mask and return from system call
+    if(semptr->scount > 0 && ) {
+    
+    }
     return OK; 
 }
