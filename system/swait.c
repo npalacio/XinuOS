7b54e2567988b7e14b758b430b706ba25ed859c8
Nick Palacio <npalacio@unomaha.edu>
2019-04-17 20:16:08 -0500

    I think this should be a working version...

--- system/swait.c
+++ system/swait.c
@@ -53,7 +53,20 @@ syscall swait(
 
             // When we get back lets run through everything again
                 // We shouldn't hit this block twice in a row since resched will only return when our process has been run again after the semA became 'good to go'
-            swait(semA, semB);
+            return swait(semA, semB);
+        }
+        if(semptrB->scount <= 0) {
+            // Block our process for this semaphore
+            semptrB->scount--;
+            prptr = &proctab[currpid];
+            prptr->prstate = PR_WAIT;   /* Set state to waiting */
+            prptr->prsem = semB;     /* Record semaphore ID  */
+            enqueue(currpid,semptrB->squeue);/* Enqueue on semaphore */
+            resched();          /*   and reschedule */
+
+            // When we get back lets run through everything again
+                // We shouldn't hit this block twice in a row since resched will only return when our process has been run again after the semA became 'good to go'
+            return swait(semA, semB);
         }
     }
     // If either semaphore is not 'good to go', block the process
