b5e2f0e785f7845cbc7fe402bb83518cf99d8e96
jallen3095 <jallen3095@gmail.com>
2019-04-18 16:28:30 -0500

    Started ssignal.c

--- system/ssignal.c
+++ system/ssignal.c
@@ -11,5 +11,44 @@ syscall ssignal(
         sid32   semB
         )
 {
+    intmask mask;           /* Saved interrupt mask         */
+    struct procent *prptr;  /* Ptr to process' table entry  */
+    struct sentry *semptrA; /* Ptr to sempahore table entry */
+    struct sentry *semptrB; /* Ptr to sempahore table entry */
+
+    // Disable interrupts
+    mask = disable();
+
+    semptrA = &semtab[semA];
+    semptrB = &semtab[semB];
+
+    // Validate the two semaphores
+    if(semA == semB) {
+        restore(mask);
+        return SYSERR;
+    }
+
+    if(isbadsem(semA) || isbadsem(semB)) {
+        restore(mask);
+        return SYSERR;
+    }
+
+    if(semptrA->sstate == S_FREE || semptrB->sstate == S_FREE) {
+        restore(mask);
+        return SYSERR;
+    }
+
+    // Increment the counts of each semaphore
+    semptrA->scount++;
+    semptrB->scount++;
+
+
+
+
+
+
+
+
+
     return OK;
 }
