245642828f4c6c4c923a106c2a85d5f2c8b6a8da
Nick Palacio <npalacio@unomaha.edu>
2019-04-17 20:00:01 -0500

    making more progress on swait

--- system/swait.c
+++ system/swait.c
@@ -10,13 +10,16 @@ syscall swait(
         )
 {
     intmask mask;           /* Saved interrupt mask     */
-    struct  sentry *semptr;     /* Ptr to sempahore table entry */
+    struct  procent *prptr;     /* Ptr to process' table entry  */
+    struct  sentry *semptrA;     /* Ptr to sempahore table entry */
+    struct  sentry *semptrB;     /* Ptr to sempahore table entry */
 
     // Disable interrupts
     mask = disable();
 
     semptrA = &semtab[semA];
     semptrB = &semtab[semB];
+
     // Validate the two semaphores
     if(semA == semB) {
         restore(mask);
@@ -31,9 +34,28 @@ syscall swait(
         return SYSERR;
     }
 
-    // If both semaphores are 'good to go' restore mask and return from system call
-    if(semptr->scount > 0 && ) {
-    
+    // If both semaphores are 'good to go', decrement, restore mask and return from system call
+    if(semptrA->scount > 0 && semptrB->scount > 0) {
+        semptrA->scount--;
+        semptrB->scount--;
+        restore(mask);
+        return OK; 
+    } else {
+        // One of the semaphores is not 'good to go'
+        if(semptrA->scount <= 0) {
+            // Block our process for this semaphore
+            semptrA->scount--;
+            prptr = &proctab[currpid];
+            prptr->prstate = PR_WAIT;   /* Set state to waiting */
+            prptr->prsem = semA;     /* Record semaphore ID  */
+            enqueue(currpid,semptrA->squeue);/* Enqueue on semaphore */
+            resched();          /*   and reschedule */
+
+            // When we get back lets run through everything again
+                // We shouldn't hit this block twice in a row since resched will only return when our process has been run again after the semA became 'good to go'
+            swait(semA, semB);
+        }
     }
+    // If either semaphore is not 'good to go', block the process
     return OK; 
 }
